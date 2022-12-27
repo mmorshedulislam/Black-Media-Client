df0de6baeb081c7034d60ed1254de84f840810fc
MORSHED-HOME <morshed952640@gmail.com>
2022-12-27 18:20:34 +0600

    All require Package added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import "flowbite";
+import Slider from "./components/Slider";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello From React!</h1>
+      <Slider></Slider>
+    </div>
+  );
+}
+
+export default App;
