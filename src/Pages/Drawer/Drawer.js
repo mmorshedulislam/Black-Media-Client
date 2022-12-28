6d14fa80f4a9ab3cb59d6e2c6f00f281efaef24a
MORSHED-HOME <morshed952640@gmail.com>
2022-12-28 19:53:44 +0600

    Post created successfully

--- src/Pages/Drawer/Drawer.js
+++ src/Pages/Drawer/Drawer.js
@@ -30,9 +30,9 @@ const Drawer = () => {
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             ></path>
           </svg>
           <span className="sr-only">Close menu</span>
@@ -72,9 +72,9 @@ const Drawer = () => {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   ></path>
                 </svg>
                 <span className="flex-1 ml-3 text-left whitespace-nowrap">
@@ -87,9 +87,9 @@ const Drawer = () => {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   ></path>
                 </svg>
               </button>
@@ -174,9 +174,9 @@ const Drawer = () => {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   ></path>
                 </svg>
                 <span className="flex-1 ml-3 whitespace-nowrap">Users</span>
@@ -195,9 +195,9 @@ const Drawer = () => {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   ></path>
                 </svg>
                 <span className="flex-1 ml-3 whitespace-nowrap">Products</span>
@@ -216,9 +216,9 @@ const Drawer = () => {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   ></path>
                 </svg>
                 <span className="flex-1 ml-3 whitespace-nowrap">Sign In</span>
@@ -237,9 +237,9 @@ const Drawer = () => {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   ></path>
                 </svg>
                 <span className="flex-1 ml-3 whitespace-nowrap">Sign Up</span>
--- src/Pages/Home/Home/CreatePost.js
+++ src/Pages/Home/Home/CreatePost.js
@@ -4,11 +4,70 @@ import { BsEmojiSmile, BsThreeDots } from "react-icons/bs";
 import { HiPhoto } from "react-icons/hi2";
 import { useContext } from "react";
 import { AuthContext } from "../../../contexts/AuthProvider";
+import { useForm } from "react-hook-form";
+import { format } from "date-fns";
 
 const CreatePost = () => {
   const { user } = useContext(AuthContext);
+  const { register, handleSubmit, reset } = useForm();
+
+  const handleCreatePost = (data) => {
+    const postText = data.postText;
+    const authorName = user?.displayName;
+    const authorImage = user?.photoURL;
+    const date = new Date();
+    const postDate = format(date, "PPPP");
+    const postMS = new Date().getMilliseconds();
+    const postImage = data.image[0];
+
+    const formData = new FormData();
+    formData.append("image", postImage);
+
+    const url = `https://api.imgbb.com/1/upload?key=b244a88f9f8d1ed1e003856b185c6459`;
+
+    fetch(url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((imgData) => {
+        if (imgData.success) {
+          const postImgUrl = imgData.data.url;
+
+          const postData = {
+            postText,
+            postImgUrl,
+            authorName,
+            authorImage,
+            postDate,
+            postMS,
+          };
+          fetch(`${process.env.REACT_APP_PORT}/posts`, {
+            method: "POST",
+            headers: {
+              "content-type": "application/json",
+            },
+            body: JSON.stringify(postData),
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              console.log(data);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
-    <form className="my-2 bg-white border rounded-lg p-3">
+    <form
+      onSubmit={handleSubmit(handleCreatePost)}
+      className="my-2 bg-white border rounded-lg p-3"
+    >
       <div className="flex gap-x-2">
         <img
           className="w-12 h-12 rounded-full"
@@ -24,6 +83,7 @@ const CreatePost = () => {
           id=""
           placeholder="What's are you Think Now?"
           className="w-full h-24 rounded-md border border-gray-300"
+          {...register("postText", { required: true })}
         ></textarea>
       </div>
       <div className="flex justify-between items-center">
@@ -40,7 +100,13 @@ const CreatePost = () => {
               <HiPhoto className="text-2xl" />
               <span>Photos</span>
             </label>
-            <input type="file" name="" id="post-img" className="hidden" />
+            <input
+              type="file"
+              name="image"
+              id="post-img"
+              className="hidden"
+              {...register("image", { required: true })}
+            />
           </div>
           <div className="flex gap-x-1 items-center justify-center cursor-pointer">
             <BsEmojiSmile className="text-2xl" />
