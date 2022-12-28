22f9bda1835275167cb2e9bebd2eac221d9a429d
MORSHED-HOME <morshed952640@gmail.com>
2022-12-28 09:01:38 +0600

    Home page icons added

--- src/Pages/NavBar/NavBar.js
+++ src/Pages/NavBar/NavBar.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { MdOutlinePermMedia } from "react-icons/md";
 import { BiSearchAlt } from "react-icons/bi";
+import { TfiUser } from "react-icons/tfi";
+import { FaHome } from "react-icons/fa";
+import { BsMessenger } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
@@ -75,24 +78,22 @@ const NavBar = () => {
           </div>
           <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <a
-                href="/"
-                class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-                aria-current="page"
-              >
-                Home
-              </a>
+              <Link to={"/"} className="text-2xl">
+                <FaHome />
+              </Link>
             </li>
             <li>
-              <a
-                href="/"
-                class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                About
-              </a>
+              <Link to={"/message"} className="text-2xl">
+                <BsMessenger />
+              </Link>
             </li>
             <li>
-              <Link to={'/media'} className="text-2xl">
+              <Link to={"/aboutme"} className="text-2xl">
+                <TfiUser />
+              </Link>
+            </li>
+            <li>
+              <Link to={"/media"} className="text-2xl">
                 <MdOutlinePermMedia />
               </Link>
             </li>
